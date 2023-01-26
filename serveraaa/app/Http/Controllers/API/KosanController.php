492ae5ba75d35fb56378a2c53cc290f68dffed35
kholidaini <kholidainii@gmail.com>
2023-01-27 06:47:55 +0700

    Update Jetstream

---
+++ serveraaa/app/Http/Controllers/API/KosanController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use App\Models\kosan;
+use Illuminate\Http\Request;
+
+class KosanController extends Controller
+{
+    public function all(Request $request){
+
+
+        $id = $request->input('id');
+        $limit = $request->input('limit');
+        $name = $request->input('id');
+        $description=$request->input('description');
+        $tags = $request->input('tags');
+        $type_id = $request->input('type_id');
+        $price_from = $request->input('price_from');
+        $price_to = $request->input('price_to');
+
+
+        if($id){
+            $kosan = kosan::with(['type_id', 'foto_kosan'])->find($id);
+            if($kosan){
+                return ResponseFormatter::success(
+                    $kosan,
+                    'databerhasildiambil'
+                );
+            }
+
+
+            else{
+                return ResponseFormatter::error(
+                    null,
+                    'datagagaldiambil',
+                    404
+                );
+            }
+        }
+
+        $kosan = kosan::with(['type_id', 'foto_kosan'])->find($id);
+
+        if($name){
+            $kosan->where('name', 'like', '%' . $name . '%');
+        }
+
+        if($description){
+            $kosan->where('description', 'like', '%' . $description . '%');
+        }
+
+        if($tags){
+            $kosan->where('tags', 'like', '%' . $tags . '%');
+        }
+
+        if($price_from){
+            $kosan->where('price','>=',$price_from);
+        }
+
+        if($price_from){
+            $kosan->where('price','<=',$price_to);
+        }
+
+        if($type_id){
+            $kosan->where('type_id',$type_id);
+        }
+
+        return ResponseFormatter::success(
+            $kosan->paginate($limit),
+            'data berhasil diambil'
+        );
+
+    }
+}
---
+++ serveraaa/database/migrations/2023_01_25_180217_create_foto_kosan_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFotoKosanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('foto_kosan', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('type_id');
+            $table->string('url');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('foto_kosan');
+    }
+}
